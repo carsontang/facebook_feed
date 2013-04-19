dc87f37a992d42e922467a2314d3d957169a7232
Carson Tang <tang.carson@gmail.com>
2013-04-18 23:33:11 -0700

    Bumped version to 1.0.4; made code more idiomatic; added tests for FeedDownloader initialization

--- test/test_feed_downloader.rb
+++ test/test_feed_downloader.rb
@@ -3,10 +3,13 @@ require 'facebook_feed'
 
 class FacebookFeedTest < Test::Unit::TestCase
   def setup
+    @valid_feed_id = 1234566
+    @valid_access_token = "ABCDEFG"
   end
 
+  # Tests for FacebookFeed::FeedDownloader initialization
   def test_valid_feed_downloader_args
-    opts = {:feed_id => 1234566, :access_token => "ABCDEFG"}
+    opts = {:feed_id => @valid_feed_id, :access_token => @valid_access_token}
     assert_nothing_raised do
       downloader = FacebookFeed::FeedDownloader.new(opts)
     end
@@ -20,9 +23,14 @@ class FacebookFeedTest < Test::Unit::TestCase
   end
 
   def test_invalid_feed_downloader_args_keys
-    opts = {:feed_id => 1234566, :access_token => "ABCDEFG", :foobar => "bogus"}
+    opts = {:feed_id => @valid_feed_id, :access_token => @valid_access_token, :foobar => "bogus"}
     assert_raise FacebookFeed::InvalidFeedDownloaderError do
       downloader = FacebookFeed::FeedDownloader.new(opts)
     end
+  end
+
+  # Tests for FacebookFeed::FeedDownloader public API
+  def test_has_more_posts_when_there_are_none
+    
   end
 end
