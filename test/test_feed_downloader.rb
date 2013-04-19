bca247255dafcb0c38311c4018e8bc19ef1d5ad1
Carson Tang <tang.carson@gmail.com>
2013-04-19 00:18:42 -0700

    Added test for structure of response hash

--- test/test_feed_downloader.rb
+++ test/test_feed_downloader.rb
@@ -5,6 +5,8 @@ class FacebookFeedTest < Test::Unit::TestCase
   def setup
     @valid_feed_id = 1234566
     @valid_access_token = "ABCDEFG"
+    @real_feed_id = 274082972642991
+    @real_access_token = "BAAE3k76AcDABAFe3Jz5qZArd3GVwurFCw2Ev5ZChVbwZBxFWpXZC90tkuKyFvIZCKKskESq3bkhknVsBKO3JKXpExgYxj7ifQ65FI7xqESPAtmIcj80XqTUvoRNEQBKVG0woIXFzmZB8qyF76txgWPYh0b8NoLbToHa0i5H5ZBdYYfYJqgCQ1dIfCeUZBs5if68SgvbXISwGBgZDZD"
   end
 
   # Tests for FacebookFeed::FeedDownloader initialization
@@ -30,7 +32,6 @@ class FacebookFeedTest < Test::Unit::TestCase
   end
 
   # Tests for FacebookFeed::FeedDownloader public API
-
   def test_has_more_posts_when_there_are_some
     opts = {:feed_id => @valid_feed_id, :access_token => @valid_access_token}
     downloader = FacebookFeed::FeedDownloader.new(opts)
@@ -42,5 +43,22 @@ class FacebookFeedTest < Test::Unit::TestCase
     downloader = FacebookFeed::FeedDownloader.new(opts)
     downloader.instance_variable_set(:@feed_urls, [])
     assert !downloader.has_more_posts?, "FacebookFeed::FeedDownloader should not have more posts."
+  end
+
+  def test_download_posts
+    opts = {:feed_id => @real_feed_id, :access_token => @real_access_token}
+    downloader = FacebookFeed::FeedDownloader.new(opts)
+    posts = downloader.download_posts if downloader.has_more_posts?
+    assert_send([posts, :any?], "posts should contains Facebook feed posts")
+  end
+
+  def test_posts_structure
+    opts = {:feed_id => @real_feed_id, :access_token => @real_access_token}
+    downloader = FacebookFeed::FeedDownloader.new(opts)
+    posts = downloader.download_posts if downloader.has_more_posts?
+    posts_keys = posts.first.keys
+    keys = %w(poster message type created_time updated_time like_count comment_count comments)
+    keys.map!(&:to_sym)
+    keys.each { |key| assert posts_keys.include?(key) }
   end
 end
